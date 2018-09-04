e1edd2bff114983e3de913bbde1b8eaf244d6df4
michammp1 <megaman-x4@hotmail.com>
2018-09-04 19:26:37 -0400

    SAS

---
+++ Assets/IAMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAMove : MonoBehaviour
+{
+    [SerializeField]
+    Transform _destination;
+    UnityEngine.AI.NavMeshAgent _navMeshAgent;
+    void Start()
+    {
+        _navMeshAgent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        if (_navMeshAgent == null)
+        {
+            Debug.LogError("El agente de navegación no esta añadido" + gameObject.name);
+        }
+        else
+        {
+            SetDestination();
+        }
+    }
+
+    private void SetDestination()
+    {
+        Vector3 targetVector = _destination.transform.position;
+        _navMeshAgent.SetDestination(targetVector);
+    }
+
+    void Update()
+    {
+
+    }
+}
